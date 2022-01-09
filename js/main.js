4d7d5e115c1c2a17732cbc21b99ad7076fd23db9
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2022-01-09 14:11:11 +0100

    Add script to scroll to top

--- js/main.js
+++ js/main.js
@@ -67,3 +67,46 @@ function changeTextLabel(i) {
         info_label[i].innerHTML = 'Click and copy';
     }, 2000);
 }
+
+// #3 Scroll to top button
+
+const scroll_button = document.getElementById('top-arrow_container');
+
+window.onscroll = function () { scroll_function() };
+
+function scroll_function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scroll_button.style.display = "flex";
+    } else {
+        scroll_button.style.display = "none";
+      }
+}
+  
+/*
+ * y: the y coordinate to scroll, 0 = top
+ * duration: scroll duration in milliseconds; default is 0 (no transition)
+ * element: the html element that should be scrolled ; default is the main scrolling element
+ */
+function scrollToY (y, duration = 0, element = document.scrollingElement) {
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
+
+    const cosParameter = document.scrollingElement.scrollTop / 2;
+    let scrollCount = 0, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollCount will be Infinity
+            scrollCount += Math.PI * (newTimestamp - oldTimestamp) / duration;
+            if (scrollCount >= Math.PI) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = cosParameter + cosParameter * Math.cos(scrollCount);
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+  }
+  
+  function scrollToTop(duration = 0) {
+    scrollToY(0, duration, document.scrollingElement);
+  }
